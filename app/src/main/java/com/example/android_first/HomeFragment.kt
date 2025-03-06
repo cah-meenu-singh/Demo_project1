ac04732b3b88fb594c515d49980346d17d109701
cah-meenu-singh <meenu.singh@cardinalhealth.com>
2025-03-06 16:12:31 +0530

    Date picker nd Tabbar

---
+++ app/src/main/java/com/example/android_first/HomeFragment.kt
@@ -0,0 +1,13 @@
+package com.example.android_first
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeActivity : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
---
+++ app/src/main/java/com/example/android_first/SettingsFragment.kt
@@ -0,0 +1,14 @@
+package com.example.android_first
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AppCompatActivity
+
+class SettingsActivity : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
